d4065fb44e110589a553da12db6ec85888c70537
= <=>
2023-05-26 11:14:03 +0700

    history

--- lib/views/calculator/calculator_view.dart
+++ lib/views/calculator/calculator_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class CalculatorView extends StatefulWidget {
   const CalculatorView({super.key});
@@ -13,6 +14,11 @@ class _CalculatorViewState extends State<CalculatorView> {
   num? angka1;
   num? angka2;
 
+  void reset() {
+    number1.text = '';
+    number2.text = '';
+  }
+
   void sum() {
     setState(() {
       if (number1.text.isNotEmpty) {
@@ -25,6 +31,7 @@ class _CalculatorViewState extends State<CalculatorView> {
       if (angka1 != null && angka2 != null) {
         setState(() {
           total = (angka1! + angka2!) as int?;
+          history.add('${number1.text} + ${number2.text} = $total');
         });
       } else {
         setState(() {
@@ -52,6 +59,7 @@ class _CalculatorViewState extends State<CalculatorView> {
       if (angka1 != null && angka2 != null) {
         setState(() {
           total = (angka1! - angka2!) as int?;
+          history.add('${number1.text} - ${number2.text} = $total');
         });
       } else {
         setState(() {
@@ -79,6 +87,7 @@ class _CalculatorViewState extends State<CalculatorView> {
       if (angka1 != null && angka2 != null) {
         setState(() {
           total = (angka1! * angka2!) as int?;
+          history.add('${number1.text} x ${number2.text} = $total');
         });
       } else {
         setState(() {
@@ -106,6 +115,7 @@ class _CalculatorViewState extends State<CalculatorView> {
       if (angka1 != null && angka2 != null) {
         setState(() {
           total = angka1! / angka2!;
+          history.add('${number1.text} / ${number2.text} = $total');
         });
       } else {
         setState(() {
@@ -121,11 +131,21 @@ class _CalculatorViewState extends State<CalculatorView> {
     print(total);
   }
 
+  final List<String> history = [];
   final number1 = TextEditingController();
   final number2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator'),
+        actions: [
+          IconButton(
+            onPressed: () => reset(),
+            icon: const Icon(Icons.restart_alt),
+          ),
+        ],
+      ),
       body: Form(
         child: SafeArea(
           child: Padding(
@@ -192,7 +212,100 @@ class _CalculatorViewState extends State<CalculatorView> {
                   visible: total != null,
                   child: Text('Total = $total'),
                 ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'History Kalkulator',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: history.isEmpty
+                      ? const CenterText()
+                      : ListHistory(
+                          history: history,
+                        ),
+                )
               ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CenterText extends StatelessWidget {
+  const CenterText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'History Masih Kosong',
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+    );
+  }
+}
+
+class ListHistory extends StatefulWidget {
+  const ListHistory({
+    super.key,
+    required this.history,
+  });
+
+  final List<String> history;
+
+  @override
+  State<ListHistory> createState() => _ListHistoryState();
+}
+
+class _ListHistoryState extends State<ListHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: ListView.builder(
+        itemCount: widget.history.length,
+        itemBuilder: (context, index) => AnimationConfiguration.staggeredList(
+          position: index,
+          duration: const Duration(milliseconds: 500),
+          child: SlideAnimation(
+            child: FadeInAnimation(
+              child: Card(
+                color: Colors.green,
+                child: ListTile(
+                  title: Text(widget.history[index]),
+                  trailing: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.history.removeAt(index);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Data berhasil Dihapus'),
+                            duration: Duration(seconds: 3),
+                          ),
+                        );
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.delete,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
