6b6cbeebd5009fc02c9119aa173d13c113ab2bd4
= <=>
2023-07-01 08:13:48 +0700

    week11

--- lib/views/authentication/splashscreen/splash_screen.dart
+++ lib/views/authentication/splashscreen/splash_screen.dart
@@ -12,7 +12,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 5), () {
-      Navigator.popAndPushNamed(context, '/ClassListScreen');
+      Navigator.popAndPushNamed(context, '/RegisterPage');
     });
   }
 
